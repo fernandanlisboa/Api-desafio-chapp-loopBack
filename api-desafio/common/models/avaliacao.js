e197e06254bda623f18d7f687904a71ba185ce7c
AmadoMaria <mariantoniamado@hotmail.com>
2021-02-18 21:42:25 -0300

    implementando "remote method" getMedidaData

--- api-desafio/common/models/avaliacao.js
+++ api-desafio/common/models/avaliacao.js
@@ -4,10 +4,45 @@ module.exports = function(Avaliacao) {
 
 //get avaliações do colaborador pela data
 //insert pegar a data e a classificação no front
-//get ultimas avaliações de cada colaborador (array com user e sua última avaliação)
+
 //get ultima avaliação user
 
 
+//get ultimas avaliações de cada colaborador (array com user e sua última avaliação) pipeline da coleção antiga
+// [
+//     {
+//       '$lookup': {
+//         'from': 'users', 
+//         'localField': 'user', 
+//         'foreignField': '_id', 
+//         'as': 'user'
+//       }
+//     }, {
+//       '$unwind': {
+//         'path': '$user'
+//       }
+//     }, {
+//       '$sort': {
+//         'dataHora': 1
+//       }
+//     }, {
+//       '$group': {
+//         '_id': '$user', 
+//         'dataUltima': {
+//           '$max': '$dataHora'
+//         }, 
+//         'pulso': {
+//           '$last': '$pulso'
+//         }, 
+//         'sistolica': {
+//           '$last': '$pressaoSistolica'
+//         }, 
+//         'diastolica': {
+//           '$last': '$pressaoDiastolica'
+//         }
+//       }
+//     }
+//   ]
 
 
 };
--- api-desafio/common/models/colaborador.js
+++ api-desafio/common/models/colaborador.js
@@ -1,9 +1,45 @@
-'use strict';
+"use strict";
 
-module.exports = function(Colaborador) {
+module.exports = function (Colaborador) {
+  //atualiza medidas (coloca as outras medidas no histórico)
+  //get medidas passando a data
 
-    //atualiza medidas (coloca as outras medidas no histórico)
-    //get medidas passando a data
-    
-
+  Colaborador.getMedidaData = async function (id, data) {
+      console.log(id)
+    return Colaborador.findById(id)
+      .then(function (colab) {
+        var colabObj = colab.toJSON();
+        for (var i = 0; i < colabObj.historicoMedidas.length; i++) {
+          if (historicoMedidas[i].dataHora == data) {
+            return historicoMedidas[i];
+          }
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+  Colaborador.remoteMethod("getMedidaData", {
+    description: "Retorna a medida de um colaborador em uma data específica",
+    accepts: [
+      {
+        arg: "data",
+        type: "date",
+        required: true,
+      },
+      {
+        arg: "id",
+        type: "string",
+        required: true,
+      },
+    ],
+    http: {
+      path: "/:id/Medida/:data",
+      verb: "get",
+    },
+    returns: {
+      arg: "medida",
+      type: "obj",
+    },
+  });
 };
