f26ca1c3f085479249607d0b97a60d2e11a7a8a2
AmadoMaria <mariantoniamado@hotmail.com>
2021-02-19 12:27:22 -0300

    criando método para atualizar medida do colaborador
    a medida anterior é adicionada ao histórico de medidas

--- api-desafio/common/models/colaborador.js
+++ api-desafio/common/models/colaborador.js
@@ -4,10 +4,11 @@ module.exports = function (Colaborador) {
   //atualiza medidas (coloca as outras medidas no histórico)
   //get medidas passando a data
 
-  Colaborador.getMedidaData = async function (id, data) {
-      console.log(id)
+  Colaborador.getMedidaData = async function (data, id) {
     return Colaborador.findById(id)
       .then(function (colab) {
+        console.log(id);
+        console.log(colab);
         var colabObj = colab.toJSON();
         for (var i = 0; i < colabObj.historicoMedidas.length; i++) {
           if (historicoMedidas[i].dataHora == data) {
@@ -40,6 +41,87 @@ module.exports = function (Colaborador) {
     returns: {
       arg: "medida",
       type: "obj",
+    },
+  });
+
+  Colaborador.updateMedida = async function (id, novaMedida) {
+    return Colaborador.findById(id).then(function (colab) {
+      var novoHistoricoMedidas = [];
+      novoHistoricoMedidas = colab.historicoMedidas;
+      novoHistoricoMedidas.push({
+        peso: colab.medida.peso,
+        altura: colab.medida.altura,
+        dataHora: colab.medida.dataHora,
+        imc: colab.medida.imc,
+      });
+
+      colab.updateAttributes(
+        {
+          medida: {
+            peso: novaMedida.peso,
+            altura: novaMedida.altura,
+            dataHora: novaMedida.dataHora,
+            imc: novaMedida.imc,
+          },
+          historicoMedidas: novoHistoricoMedidas,
+        },
+        function (err, instance) {
+          console.log(instance);
+        }
+      );
+    });
+  };
+  Colaborador.remoteMethod("updateMedida", {
+    description:
+      "Atualiza a medida de um colaborador e adiciona a antiga ao histórico",
+    accepts: [
+      {
+        arg: "id",
+        type: "string",
+        required: true,
+      },
+      {
+        arg: "novaMedida",
+        type: {
+          peso: {
+            type: "number",
+          },
+          altura: {
+            type: "number",
+          },
+          dataHora: {
+            type: "date",
+          },
+          imc: {
+            type: "number",
+          },
+        },
+        required: true,
+        http: {
+          source: "body",
+        },
+      },
+    ],
+    http: {
+      path: "/:id/Medida",
+      verb: "put",
+    },
+    returns: {
+      arg: "novaMedida",
+      type: {
+        peso: {
+          type: "number",
+        },
+        altura: {
+          type: "number",
+        },
+        dataHora: {
+          type: "date",
+        },
+        imc: {
+          type: "number",
+        },
+      },
     },
   });
 };
