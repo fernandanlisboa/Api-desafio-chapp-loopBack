3875f55c8012fef688da6c4ede943299edb56111
AmadoMaria <mariantoniamado@hotmail.com>
2021-02-21 14:47:19 -0300

    adicionando métodos para buscar a última avaliação de um colaborador específico e a última avaliação de cada um dos colaboradores

--- api-desafio/common/models/avaliacao.js
+++ api-desafio/common/models/avaliacao.js
@@ -1,48 +1,133 @@
-'use strict';
+"use strict";
 
-module.exports = function(Avaliacao) {
+module.exports = function (Avaliacao) {
+  //get avaliações do colaborador pela data 
+    // (já existe "/colaboradors/{id}/avaliacoes", 
+    //só é necessário aplicar o filtro: {"where":{"dataHora": "2021-02-21T16:34:08.081Z"}})
 
-//get avaliações do colaborador pela data
-//insert pegar a data e a classificação no front
+  //insert pegar a data e a classificação no front
 
-//get ultima avaliação user
+  //get ultima avaliação user (ok)
+  //get ultima avaliação de cada colaborador (Array) (ok)
 
+  Avaliacao.getUltimaAvaliacao = async function (colaboradorId) {
+    return Avaliacao.aggregate({
+      where: { colaboradorId: colaboradorId },
+      aggregate: [
+        {
+          $sort: {
+            dataHora: 1,
+          },
+        },
+        {
+          $group: {
+            _id: "$colaboradorId",
+            dataUltima: {
+              $max: "$dataHora",
+            },
+            pulso: {
+              $last: "$pulso",
+            },
+            pSistolica: {
+              $last: "$pSistolica",
+            },
+            pDiastolica: {
+              $last: "$pDiastolica",
+            },
+            hipertensao: {
+              $last: "$hipertensao",
+            },
+          },
+        },
+      ],
+    })
+      .then(function (avaliacao) {
+        return Promise.resolve(avaliacao);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
 
-//get ultimas avaliações de cada colaborador (array com user e sua última avaliação) pipeline da coleção antiga
-// [
-//     {
-//       '$lookup': {
-//         'from': 'users', 
-//         'localField': 'user', 
-//         'foreignField': '_id', 
-//         'as': 'user'
-//       }
-//     }, {
-//       '$unwind': {
-//         'path': '$user'
-//       }
-//     }, {
-//       '$sort': {
-//         'dataHora': 1
-//       }
-//     }, {
-//       '$group': {
-//         '_id': '$user', 
-//         'dataUltima': {
-//           '$max': '$dataHora'
-//         }, 
-//         'pulso': {
-//           '$last': '$pulso'
-//         }, 
-//         'sistolica': {
-//           '$last': '$pressaoSistolica'
-//         }, 
-//         'diastolica': {
-//           '$last': '$pressaoDiastolica'
-//         }
-//       }
-//     }
-//   ]
+  Avaliacao.remoteMethod("getUltimaAvaliacao", {
+    description: "Retorna a última avaliação de um colaborador",
+    accepts: [
+      {
+        arg: "colaboradorId",
+        type: "string",
+        required: true,
+      },
+    ],
+    http: {
+      path: "/ultima/colaboradorId",
+      verb: "get",
+    },
+    returns: {
+      type: Avaliacao,
+      root: true,
+    },
+  });
 
+  Avaliacao.getUltimasAvaliacoes = async function () {
+    return Avaliacao.aggregate({
+      aggregate: [
+        {
+          $lookup: {
+            from: "colaborador",
+            localField: "colaboradorId",
+            foreignField: "_id",
+            as: "colaborador",
+          },
+        },
+        {
+          $unwind: {
+            path: "$colaborador",
+          },
+        },
+        {
+          $sort: {
+            dataHora: 1,
+          },
+        },
+        {
+          $group: {
+            _id: "$colaboradorId",
+            dataUltima: {
+              $max: "$dataHora",
+            },
+            pulso: {
+              $last: "$pulso",
+            },
+            pSistolica: {
+              $last: "$pSistolica",
+            },
+            pDiastolica: {
+              $last: "$pDiastolica",
+            },
+            hipertensao: {
+              $last: "$hipertensao",
+            },
+          },
+        },
+      ],
+    })
+      .then(function (avaliacoes) {
+        return Promise.resolve(avaliacoes);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
 
+  Avaliacao.remoteMethod("getUltimasAvaliacoes", {
+    description: "Retorna a última avaliação de todos os colaboradores",
+    http: {
+      path: "/ultimas",
+      verb: "get",
+    },
+    returns: {
+      type: [Avaliacao],
+      root: true,
+    },
+  });
 };
